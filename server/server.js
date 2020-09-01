7d2294d3cb7a6b15632625b43a988c9d1dc16f40
Ethan Kavanagh <ethanmkavanagh@outlook.com>
2020-09-01 15:31:23 -0500

    serve static assets

--- server/server.js
+++ server/server.js
@@ -6,7 +6,16 @@ const express = require( `express` );
 // Makes an app, aka a "server"
 const app = express();
 
+// Incantation for serving static files
+app.use( express.static( `server/public` ) );
+
+// When I get a request at "/hello"
+// send back a response of "Hello World"
+app.get(`/hello`, function( req, res ){
+    res.send();
+} );
+
 // Start up the server
 app.listen( 3000, function(){
-    console.log( `I'm listening` );
+    console.log( `I'm listening on 3000` );
 } );
